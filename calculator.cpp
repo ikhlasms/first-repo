9e8e8d9ae1b8c7fb8e7a6289b284db7a0228699b
Ikhlas Mohammad <102763381+ikhlasms@users.noreply.github.com>
2022-04-07 13:13:05 -0700

    First file ....
    I uploaded this file to be more active in coding.
    this file is about a simple  calculator by using C++.

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void addition();
+void subtruction();
+void multiplication();
+void division();
+void factorial();
+void power();
+void square();
+void cube();
+void squareroot();
+
+int main()
+{
+    cout<<"\t\tWellcome to scientific calculator\n";
+
+
+    return 0;
+}
